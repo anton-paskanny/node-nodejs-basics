fbfd4e612a07864a1056242f035868899a10d165
Anton Paskanny <anton.paskanny@gmail.com>
2022-06-05 23:38:14 +0300

    feat/implement create function (fs)

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,3 +1,32 @@
+//const fs = require('fs/promises');
+import fs from 'fs/promises';
+
+// async function exists (path) {  
+//     try {
+//       await fs.access(path);
+//       return true;
+//     } catch {
+//         ///throw new Error('FS operation failed');
+//         console.log('return false!!!')
+//       return false;
+//     }
+//   }
+
 export const create = async () => {
-    // Write your code here 
+    // Write your code here
+    const fileContent = 'I am fresh and younasd asdas dg';
+    const fileName = 'fresh.txt';
+    const filePath = `./files/${fileName}`;
+    const fileExistsErrText = 'FS operation failed - such file already exists!';
+
+    try {
+        const data = await fs.readFile(filePath, { encoding: 'utf8' });
+        if (data) { throw new Error(fileExistsErrText); }
+    } catch (e) {
+        if (e.message !== fileExistsErrText) {
+            await fs.writeFile(filePath, fileContent);
+        }
+    }
 };
+
+create();
