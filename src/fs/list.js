5b24886984cb90b846cf25630eae9a650157f8ce
Anton Paskanny <anton.paskanny@gmail.com>
2022-06-09 00:09:24 +0300

    feat/implement-list-method

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,3 +1,16 @@
+import { readdir } from 'node:fs/promises';
+
 export const list = async () => {
-    // Write your code here 
+    const sourceFolderPath = './files';
+
+    try {
+        const filesToCopy = await readdir(sourceFolderPath);
+        for (const file of filesToCopy) { 
+            console.log(file);
+        };
+    } catch {
+        throw new Error('FS operation failed');
+    }
 };
+
+list();
