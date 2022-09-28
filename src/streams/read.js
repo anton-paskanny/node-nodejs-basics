16b387eaad5395a5effe19fbefea002700cf0448
Anton Paskanny <anton.paskanny@gmail.com>
2022-09-28 13:56:44 +0300

    feat/implement read stream method

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,3 +1,12 @@
+import fs from 'fs';
+
 export const read = async () => {
     // Write your code here 
+    const stream = fs.createReadStream('./files/fileToRead.txt');
+
+    stream.on('error', err => console.log(err));
+
+    stream.on('data', chunk => process.stdout.write(chunk));
 };
+
+read();
