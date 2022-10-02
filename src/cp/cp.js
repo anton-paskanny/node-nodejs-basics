2db95520b3b851efa8930d355066916193af9e37
Anton Paskanny <anton.paskanny@gmail.com>
2022-10-02 21:22:15 +0300

    feat/implement cp

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,3 +1,15 @@
+import { spawn } from 'child_process';
+
 export const spawnChildProcess = async (args) => {
-    // Write your code here
+    const chProcess = spawn('node', [`./files/script.js`, ...args.split(' ')]);
+    
+    process.stdin.on('data', data => chProcess.stdin.write(data));
+
+    chProcess.stdout.on('data', data => {
+        console.log(data.toString());
+        process.stdout.write(data);
+    });
 };
+
+
+spawnChildProcess('--version --kek');
